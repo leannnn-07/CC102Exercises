ad0c3883ab0b911fb15b248cdb59d3bce881a8ec
leannnn-07 <ty202501099@wmsu.edu.ph>
2026-02-16 18:08:59 +0800

    Add files via upload

---
+++ BajeCC102Lab4_1.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char choice;
+
+    do {
+        int students, quizzes;
+
+        cout << "Enter number of students: ";
+        cin >> students;
+
+        cout << "Enter number of quizzes: ";
+        cin >> quizzes;
+
+        double scores[100][100];
+        double average[100];
+
+        // Input scores
+        for (int i = 0; i < students; i++) {
+            cout << "\nStudent " << i + 1 << " scores:\n";
+            double sum = 0;
+
+            for (int j = 0; j < quizzes; j++) {
+                cout << "Quiz " << j + 1 << ": ";
+                cin >> scores[i][j];
+                sum += scores[i][j];
+            }
+
+            average[i] = sum / quizzes;
+        }
+
+        // Display table header
+        cout << "\nStudent\t";
+        for (int j = 0; j < quizzes; j++) {
+            cout << "Q" << j + 1 << "\t";
+        }
+        cout << "Average\n";
+
+        // Display table content
+        for (int i = 0; i < students; i++) {
+            cout << i + 1 << "\t";
+
+            for (int j = 0; j < quizzes; j++) {
+                cout << scores[i][j] << "\t";
+            }
+
+            cout << average[i] << endl;
+        }
+
+        cout << "\nDo you want to run the program again? (y/n): ";
+        cin >> choice;
+
+    } while (choice == 'y' || choice == 'Y');
+
+    return 0;
+}
---
+++ BajeCC102Lab4_2.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char choice;
+
+    do {
+        double sales[5][4] = {0};
+        int salesperson, product;
+        double amount;
+        char more;
+
+        // Input sales records
+        do {
+            cout << "\nEnter Salesperson number (1-4): ";
+            cin >> salesperson;
+
+            cout << "Enter Product number (1-5): ";
+            cin >> product;
+
+            cout << "Enter Amount sold: ";
+            cin >> amount;
+
+            if (salesperson >= 1 && salesperson <= 4 &&
+                product >= 1 && product <= 5) {
+                sales[product - 1][salesperson - 1] += amount;
+            } else {
+                cout << "Invalid input. Try again.\n";
+            }
+
+            cout << "Enter another record? (y/n): ";
+            cin >> more;
+
+        } while (more == 'y' || more == 'Y');
+
+        cout << "\n\t\tSalesperson\n";
+        cout << "Product\t1\t2\t3\t4\tTotal\n";
+
+        double grandTotal = 0;
+
+        // Display table with row totals
+        for (int i = 0; i < 5; i++) {
+            double rowTotal = 0;
+            cout << i + 1 << "\t";
+
+            for (int j = 0; j < 4; j++) {
+                cout << sales[i][j] << "\t";
+                rowTotal += sales[i][j];
+            }
+
+            cout << rowTotal << endl;
+            grandTotal += rowTotal;
+        }
+
+        // Column totals
+        cout << "Total\t";
+        for (int j = 0; j < 4; j++) {
+            double columnTotal = 0;
+            for (int i = 0; i < 5; i++) {
+                columnTotal += sales[i][j];
+            }
+            cout << columnTotal << "\t";
+        }
+
+        cout << grandTotal << endl;
+
+        cout << "\nDo you want to run the program again? (y/n): ";
+        cin >> choice;
+
+    } while (choice == 'y' || choice == 'Y');
+
+    return 0;
+}
